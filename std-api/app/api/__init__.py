940df3ed366aa1c53c87a84a10bcd62539b42c57
minsuhya <minsuhya@gmail.com>
2024-05-04 17:18:30 +0900

    remove mongodb

--- std-api/app/api/__init__.py
+++ std-api/app/api/__init__.py
@@ -1,11 +1,11 @@
 from .auth import router as auth_router
-from .users import router as user_router
 from .books import router as book_router
-from .files import router as file_router
-from .heroes import router as hero_router
+from .graphql import router as graphql_router
+# from .files import router as file_router
+# from .heroes import router as hero_router
 from .teams import router as team_router
 from .tutorials import router as tutorial_router
-from .graphql import router as graphql_router
+from .users import router as user_router
 
 __all__ = [
     auth_router,
@@ -13,7 +13,7 @@ __all__ = [
     hero_router,
     team_router,
     tutorial_router,
-    file_router,
-    book_router,
+    # file_router,
+    # book_router,
     graphql_router,
 ]
