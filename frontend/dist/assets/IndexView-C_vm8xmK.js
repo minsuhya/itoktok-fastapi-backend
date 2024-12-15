e7c9b97d2bd312e92dfe74d84b53eff0dbb61fca
rupi <minsuhya@gmail.com>
2024-12-15 23:47:17 +0900

    frontend build

--- frontend/dist/assets/IndexView-C_vm8xmK.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-Cmu6_IW5.js";const n={};function r(c,t){return null}const o=e(n,[["render",r]]);export{o as default};
--- frontend/dist/assets/test--nUQML7d.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./index-Cmu6_IW5.js";function t(r){return r={email:"eve.holt@reqres.in",password:"cityslicka"},e.post("https://reqres.in/api/register",r)}function i(r){return e.get("https://reqres.in/api/users?page="+r)}export{i as l,t as r};
